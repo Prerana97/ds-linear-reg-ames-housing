b3462b75b5462bd8dc8509703a07657b68909623
Prerana97 <prerana12897@gmail.com>
2022-06-22 06:53:44 +0000

    update ap.py

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ myheading1='Predicting Home Sale Prices in Ames, Iowa'
 image1='ames_welcome.jpeg'
 tabtitle = 'Ames Housing'
 sourceurl = 'http://jse.amstat.org/v19n3/decock.pdf'
-githublink = 'https://github.com/plotly-dash-apps/501-linear-reg-ames-housing'
+githublink = 'https://github.com/Prerana97/ds-linear-reg-ames-housing'
 
 
 ########### Initiate the app
@@ -44,7 +44,7 @@ app.layout = html.Div(children=[
             html.Div([
                 html.Button(children='Submit', id='submit-val', n_clicks=0,
                                 style={
-                                'background-color': 'red',
+                                'background-color': 'blue',
                                 'color': 'white',
                                 'margin-left': '5px',
                                 'verticalAlign': 'center',
@@ -59,7 +59,7 @@ app.layout = html.Div(children=[
     html.Br(),
     html.Br(),
     html.H4('Regression Equation:'),
-    html.Div('Predicted Price = (- $1,360.5K Baseline) + ($0.7K * Year Built) + ($12.7K * Bathrooms) + (- $7.7K * Bedrooms) + ($0.049K * Total Square Feet) + ($ 25.2K * Single Family Home) + (- $6.6 K * Large Neighborhood) + ( 2057 * Fireplaces)'),
+    html.Div('Predicted Price = (- $1,360.5K Baseline) + ($0.7K * Year Built) + ($12.7K * Bathrooms) + (- $7.7K * Bedrooms) + ($0.049K * Total Square Feet) + ($ 25.2K * Single Family Home) + (- $6.6 K * Large Neighborhood) + ($2K * Fireplaces)'),
     html.Br(),
     html.A('Google Spreadsheet', href='https://docs.google.com/spreadsheets/d/1q2ustRvY-GcmPO5NYudvsBEGNs5Na5p_8LMeS4oM35U/edit?usp=sharing'),
     html.Br(),
